1ea581dd215d54fb2a6a8d420ab26018686f99ea
Todor Shestakov <to.shestakov@gmail.com>
2022-03-27 21:07:08 +0300

    Adding new projects

---
+++ CSharp-Programming-basics/03ConditionalStatementsAdvanced/08CinemaTicket/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _8.Cinema_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//VAR 1
+            //string weekDay = Console.ReadLine();
+
+            //int price = 0;
+
+            //switch (weekDay)
+            //{
+            //    case "Monday":
+            //    case "Tuesday":
+            //        price = 12;
+            //        break;
+            //    case "Wednesday":
+            //    case "Thursday":
+            //        price = 14;
+            //        break;
+            //    case "Friday":
+            //        price = 12;
+            //        break;
+            //    case "Saturday":
+            //    case "Sunday":
+            //        price = 16;
+            //        break;
+            //}
+
+            //Console.WriteLine(price);
+
+//VAR 2
+            ////Monday Tuesday Wednesday Thursday    Friday Saturday    Sunday
+            ////12       12        14      14          12      16         16
+            
+            string weekDay = Console.ReadLine();
+
+            if (weekDay == "Monday" || weekDay == "Tuesday" || weekDay == "Friday")
+            {
+                Console.WriteLine(12);
+            }
+            else if (weekDay == "Wednesday" || weekDay == "Thursday")
+            {
+                Console.WriteLine(14);
+            }
+            else if (weekDay == "Saturday" || weekDay == "Sunday")
+            {
+                Console.WriteLine(16);
+            }
+
+        }
+    }
+}
