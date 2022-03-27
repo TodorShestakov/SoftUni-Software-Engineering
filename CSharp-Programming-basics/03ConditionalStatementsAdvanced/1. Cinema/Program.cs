b4b9d91afb11bdc23d7408cccf4cdf7e4e0c38b4
Todor Shestakov <to.shestakov@gmail.com>
2022-03-27 13:38:57 +0300

    Added Cinema project

---
+++ CSharp-Programming-basics/03ConditionalStatementsAdvanced/1. Cinema/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _1._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+//VAR 1
+            //string name = Console.ReadLine();
+            //int rows = int.Parse(Console.ReadLine());
+            //int columns = int.Parse(Console.ReadLine());
+
+            //int capacity = rows * columns;
+
+            //double price = 0;
+
+            //if (name == "Premiere")
+            //{
+            //    price = 12.00;
+            //}
+            //else if (name == "Normal")
+            //{
+            //    price = 7.50;
+            //}
+            //else if (name == "Discount")
+            //{
+            //    price = 5.00;
+            //}
+
+            //double total = price * capacity;
+
+            //Console.WriteLine($"{total:f2} leva");
+
+
+
+//VAR 2
+            string name = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            int capacity = rows * columns;
+
+            double price = 0;
+
+            switch (name)
+            {
+                case "Premiere":
+                    price = 12.00;
+                    break;
+                case "Normal":
+                    price = 7.50;
+                    break;
+                case "Discount":
+                    price = 5.00;
+                    break;
+            }
+
+            double total = price * capacity;
+
+            Console.WriteLine($"{total:f2} leva");
+
+
+        }
+    }
+}
