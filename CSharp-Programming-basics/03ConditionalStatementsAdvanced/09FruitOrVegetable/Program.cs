0d8977e29ccc5d59d3cf440226f253eb91fb5606
Todor Shestakov <to.shestakov@gmail.com>
2022-03-27 23:36:04 +0300

    Added Fruit or Vegetable project

---
+++ CSharp-Programming-basics/03ConditionalStatementsAdvanced/09FruitOrVegetable/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _9._Fruit_or_Vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// VAR 1
+            //string name = Console.ReadLine();
+            //switch (name)
+            //{
+            //    case "banana":
+            //    case "apple":
+            //    case "kiwi":
+            //    case "cherry":
+            //    case "lemon":
+            //    case "grapes":
+            //        Console.WriteLine("fruit");
+            //        break;
+            //    case "tomato":
+            //    case "cucumber":
+            //    case "pepper":
+            //    case "carrot":
+            //        Console.WriteLine("vegetable");
+            //        break;
+            //    default:
+            //        Console.WriteLine("unknown");
+            //        break;            
+            //}
+
+
+
+// VAR 2
+            string name = Console.ReadLine();
+            
+
+            if (name == "banana")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (name == "apple")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (name == "kiwi")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (name == "cherry")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (name == "lemon")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (name == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (name == "tomato")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else if (name == "cucumber")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else if (name == "pepper")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else if (name == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+
+
+        }
+    }
+}
