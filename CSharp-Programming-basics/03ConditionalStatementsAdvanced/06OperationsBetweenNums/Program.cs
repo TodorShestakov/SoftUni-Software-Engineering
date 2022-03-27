40b0febe092276e805eb28b2d41bf6a69503dadb
Todor Shestakov <to.shestakov@gmail.com>
2022-03-27 19:39:06 +0300

    Adding new projects

---
+++ CSharp-Programming-basics/03ConditionalStatementsAdvanced/06OperationsBetweenNums/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace _6.OperationsBetweenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            string operatoR = Console.ReadLine();
+
+            double operation = 0;
+
+            if (operatoR == "+")
+            {
+                operation = n1 + n2;
+                if (operation % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} + {n2} = {operation} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} + {n2} = {operation} - odd");
+                }
+            }
+            else if (operatoR == "-")
+            {
+                operation = n1 - n2;
+                if (operation % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} - {n2} = {operation} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} - {n2} = {operation} - odd");
+                }
+            }
+            else if (operatoR == "*")
+            {
+                operation = n1 * n2;
+                if (operation % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} * {n2} = {operation} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} * {n2} = {operation} - odd");
+                }
+            }
+            else if (operatoR == "/")
+            {
+                if (n2 != 0)
+                {
+                    operation = n1 / n2;
+                    Console.WriteLine($"{n1} / {n2} = {n1 * 1.0 / n2:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+                
+            }
+            else if (operatoR == "%")
+            {
+                if (n2 != 0)
+                {
+                    operation = n1 % n2;
+                    Console.WriteLine($"{n1} % {n2} = {operation}");
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+            }
+            
+        }
+    }
+}
---
+++ CSharp-Programming-basics/03ConditionalStatementsAdvanced/06TruckDriver/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _6._Truck_Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            double kmPerMonth = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (kmPerMonth <= 5000)
+            {
+                if ((season == "Spring") || (season == "Autumn"))
+                {
+                    price = (kmPerMonth * 4) * 0.75;
+                }
+                else if (season == "Summer")
+                {
+                    price = (kmPerMonth * 4) * 0.90;
+                }
+                else if (season == "Winter")
+                {
+                    price = (kmPerMonth * 4) * 1.05;
+                }
+            }
+            else if ((kmPerMonth > 5000) && (kmPerMonth <= 10000))
+            {
+                if ((season == "Spring") || (season == "Autumn"))
+                {
+                    price = (kmPerMonth * 4) * 0.95;
+                }
+                else if (season == "Summer")
+                {
+                    price = (kmPerMonth * 4) * 1.10;
+                }
+                else if (season == "Winter")
+                {
+                    price = (kmPerMonth * 4) * 1.25;
+                }
+            }
+            else if ((kmPerMonth > 10000) && (kmPerMonth <= 20000))
+            {
+                price = (kmPerMonth * 4) * 1.45;               
+            }
+
+            price *= 0.90;
+
+            Console.WriteLine($"{price:f2}");
+
+        }
+    }
+}
