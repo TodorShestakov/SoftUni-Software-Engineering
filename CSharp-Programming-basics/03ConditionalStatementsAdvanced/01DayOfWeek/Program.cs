9b1f499790fe3d0c37ef00ed37d8d931ba98bf45
TodorShestakov <102318727+TodorShestakov@users.noreply.github.com>
2022-03-27 13:48:09 +0300

    Update Program.cs

--- CSharp-Programming-basics/03ConditionalStatementsAdvanced/01DayOfWeek/Program.cs
+++ CSharp-Programming-basics/03ConditionalStatementsAdvanced/01DayOfWeek/Program.cs
@@ -7,35 +7,35 @@ namespace _1._Day_of_Week
         static void Main(string[] args)
         {
 //Var 1
-            //int number = int.Parse(Console.ReadLine());
-
-            //switch (number)
-            //{
-            //    case 1:
-            //        Console.WriteLine("Monday");
-            //        break;
-            //    case 2:
-            //        Console.WriteLine("Tuesday");
-            //        break;
-            //    case 3:
-            //        Console.WriteLine("Wednesday");
-            //        break;
-            //    case 4:
-            //        Console.WriteLine("Thursday");
-            //        break;
-            //    case 5:
-            //        Console.WriteLine("Friday");
-            //        break;
-            //    case 6:
-            //        Console.WriteLine("Saturday");
-            //        break;
-            //    case 7:
-            //        Console.WriteLine("Sunday");
-            //        break;
-            //    default:
-            //        Console.WriteLine("Error");
-            //        break;
-            //}
+            int number = int.Parse(Console.ReadLine())
+                
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
 
 //Var 2
             //int number = int.Parse(Console.ReadLine());
