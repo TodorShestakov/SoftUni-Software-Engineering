e2c6bc218087b3d76920d73c596c18859baeae4e
Todor Shestakov <to.shestakov@gmail.com>
2022-03-28 23:29:22 +0300

    Added Odd Even Sum project

---
+++ CSharp-Programming-basics/04ForLoop/10OddEvenSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sum1 += element;
+                }
+                else
+                {
+                    sum2 += element;
+                }
+            }
+
+            
+            int difference = Math.Abs(sum1 - sum2);
+
+            if (sum1 == sum2)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + sum1);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + difference);
+            }
+
+
+        }
+    }
+}
