95932af5c4eda08a589983c14d470c867880a0c0
Todor Shestakov <to.shestakov@gmail.com>
2022-03-29 00:32:27 +0300

    Adding Old Books project

---
+++ CSharp-Programming-basics/05WhileLoop/01OldBooks/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _5._While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string bookName = Console.ReadLine();
+
+            //string currentBook = Console.ReadLine();
+
+            //int counter = 0;
+
+            //while (currentBook != bookName)
+            //{
+            //    counter++;
+            //    currentBook = Console.ReadLine();
+
+            //    if (currentBook == "No More Books")
+            //    {
+            //        break;
+            //    }
+            //}
+            //if (currentBook == bookName)
+            //{
+            //    Console.WriteLine($"You checked {counter} books and found it.");
+            //}
+            //else if (currentBook != bookName)
+            //{
+            //    Console.WriteLine($"The book you search is not here!");
+            //    Console.WriteLine($"You checked {counter} books.");
+            //}
+
+
+//VAR 2
+            string wantedBook = Console.ReadLine();
+
+            string currentBook = Console.ReadLine();
+            int counter = 0;
+
+            while (currentBook != "No More Books")
+            {
+
+                if (currentBook == wantedBook)
+                {
+                    Console.WriteLine($"You checked {counter} books and found it.");
+                    break;
+                }
+                counter++;
+                currentBook = Console.ReadLine();
+            }
+            if (currentBook == "No More Books")
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+        }
+    }
+}
