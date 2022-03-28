f7ed81933b8042f55bdc24590bf33a513198530e
Todor Shestakov <to.shestakov@gmail.com>
2022-03-28 14:38:34 +0300

    Adding new projects

---
+++ CSharp-Programming-basics/04ForLoop/04CleverLily/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _4._Clever_Lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //•	Възрастта на Лили - цяло число в интервала[1...77]
+            //•	Цената на пералнята - число в интервала[1.00...10 000.00]
+            //•	Единична цена на играчка -цяло число в интервала[0...40]
+
+
+            int age = int.Parse(Console.ReadLine());
+            double peralnqPrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            int moneyYears = 0;
+            int money = 0;
+            int savedMoney = 0;
+            int toyYears = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    moneyYears += 1;
+                    money = moneyYears * 10;
+                    savedMoney += money; 
+                }
+                else
+                {
+                    toyYears++;
+                }
+                
+            }
+
+
+            money = savedMoney - moneyYears + (toyYears * toyPrice);
+
+            double difference = Math.Abs(money - peralnqPrice);
+
+            if (money >= peralnqPrice)
+            {
+                Console.WriteLine($"Yes! {difference:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {difference:f2}");
+            }
+
+
+        }
+    }
+}
---
+++ CSharp-Programming-basics/04ForLoop/04EvenPowersOf2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+    //var 1
+            //for (int power = 0; power <= n; power += 2)
+            //{
+            //    Console.WriteLine(Math.Pow(2, power));
+            //}
+
+
+    //var 2
+            int num = 1;
+            for (int i = 0; i <= n; i+= 2)
+            {
+                Console.WriteLine(num);
+                num *= 2 * 2;
+            }
+        }
+    }
+}
