449b10841288f9c0092ee1636b0a473e3c45bf92
Todor Shestakov <to.shestakov@gmail.com>
2022-03-28 10:57:13 +0300

    Added Back To The Past project

---
+++ CSharp-Programming-basics/04ForLoop/02BackToThePast/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double money = double.Parse(Console.ReadLine());
+            int yearsToLive = int.Parse(Console.ReadLine());
+
+            double age = 18;
+
+            for (int i = 1800; i <= yearsToLive; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    age++;
+                    money -= 12000;
+                }
+                else
+                {
+
+                    money -= 12000 + age * 50;
+                    age++;
+                }
+            }
+
+
+            double diff = Math.Abs(0 - money);
+
+            if (money >= 0)
+            {
+                Console.WriteLine($"Yes! He will live a carefree life and will have {diff:f2} dollars left.");
+            }
+            else
+            {
+                Console.WriteLine($"He will need {diff:f2} dollars to survive.");
+            }
+        }
+    }
+}
