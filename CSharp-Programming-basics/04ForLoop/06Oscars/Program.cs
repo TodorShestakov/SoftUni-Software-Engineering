86c45957db9bb2f8a1fde02a318b8b9069b9a076
Todor Shestakov <to.shestakov@gmail.com>
2022-03-28 16:30:00 +0300

    Added new projects

---
+++ CSharp-Programming-basics/04ForLoop/06Oscars/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _6._Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string actorName = Console.ReadLine();
+            double startingPoints = double.Parse(Console.ReadLine());
+            int judgesCount = int.Parse(Console.ReadLine());
+
+            double totalPoints = startingPoints;
+
+            for (int i = 0; i < judgesCount; i++)
+            {
+                string judge = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+
+
+                double momentPoints = (judge.Length * points) / 2;
+                totalPoints += momentPoints;
+
+                if (totalPoints >= 1250.5)
+                {
+                    break;
+                }
+
+            }
+
+            double diff = Math.Abs(totalPoints - 1250.5);
+
+            if (totalPoints >= 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {actorName} got a nominee for leading role with {totalPoints:f1}!");
+            }
+
+
+            else if (totalPoints <= 1250.5)
+            {
+                Console.WriteLine($"Sorry, {actorName} you need {diff:f1} more!");
+            }
+
+
+        }
+    }
+}
---
+++ CSharp-Programming-basics/04ForLoop/06VowelsSum/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _6._Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string text = Console.ReadLine();
+            //int sum = 0;
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    char currentSymbol = text[i];
+
+            //    if (currentSymbol == 'a')
+            //    {
+            //        sum += 1;
+            //    }
+            //    else if (currentSymbol == 'e')
+            //    {
+            //        sum += 2;
+            //    }
+            //    else if (currentSymbol == 'i')
+            //    {
+            //        sum += 3;
+            //    }
+            //    else if (currentSymbol == 'o')
+            //    {
+            //        sum += 4;
+            //    }
+            //    else if (currentSymbol == 'u')
+            //    {
+            //        sum += 5;
+            //    }
+            //}
+            
+            //Console.WriteLine(sum);
+
+
+            int suma = 0;
+            int sume = 0;
+            int sumi = 0;
+            int sumo = 0;
+            int sumu = 0;
+            string s = Console.ReadLine();
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (s[i] == 'a')
+                    suma++;
+                else if (s[i] == 'e')
+                    sume += 2;
+                else if (s[i] == 'i')
+                    sumi += 3;
+                else if (s[i] == 'o')
+                    sumo += 4;
+                else if (s[i] == 'u')
+                    sumu += 5;
+            }
+            int ResultSum = suma + sume + sumi + sumo + sumu;
+            Console.WriteLine(ResultSum);
+        }
+    }
+}
