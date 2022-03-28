48c5ad5dc8855283724c9d2d62eac0c20a9b4e4b
Todor Shestakov <to.shestakov@gmail.com>
2022-03-28 18:38:59 +0300

    Added Number Sequence project

---
+++ CSharp-Programming-basics/04ForLoop/08NumberSequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _8._Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberOfRows = int.Parse(Console.ReadLine());
+
+            int maxN = int.MinValue;
+            int minN = int.MaxValue;
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > maxN)
+                {
+                    maxN = num;
+                }
+                if (num < minN)
+                {
+                    minN = num;
+                }
+            }
+
+           Console.WriteLine("Max number: " + maxN);
+           Console.WriteLine("Min number: " + minN);
+
+        }
+    }
+}
