bb6309263245cb35df1caeac9580830a39f6bf00
Todor Shestakov <to.shestakov@gmail.com>
2022-03-28 11:52:29 +0300

    Added new projects

---
+++ CSharp-Programming-basics/04ForLoop/02.HalfSumElement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _2._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                sum += numbers;
+                if (numbers > max)
+                {
+                    max = numbers;
+                }
+            }
+
+            int sumWithoutMax = sum - max;
+
+            if (sumWithoutMax == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + max);
+            }
+            else
+            {
+                int difference = Math.Abs(sumWithoutMax - max);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + difference);
+            }
+        }
+    }
+}
---
+++ CSharp-Programming-basics/04ForLoop/02Hospital/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int period = int.Parse(Console.ReadLine());
+
+            int doctors = 7;
+            int nepregledani = 0;
+            int pregledani = 0;
+
+            for (int i = 1; i <= period; i++)
+            {
+                int numberOfPatients = int.Parse(Console.ReadLine());
+
+                if (i % 3 == 0)
+                {
+                    if (nepregledani > pregledani)
+                    {
+                        doctors++;
+                    }
+
+                }
+
+                if (numberOfPatients > doctors)
+                {
+                    pregledani += doctors;
+                    nepregledani += numberOfPatients - doctors;
+                }
+
+                else
+                {
+                    pregledani += numberOfPatients;
+                }
+            }
+
+            Console.WriteLine($"Treated patients: {pregledani}.");
+            Console.WriteLine($"Untreated patients: {nepregledani}.");
+        }
+    }
+}
