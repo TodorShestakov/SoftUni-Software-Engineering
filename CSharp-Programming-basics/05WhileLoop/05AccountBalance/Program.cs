1b113f7931c15e9fe4647529b0ab280ddbb77f21
Todor Shestakov <to.shestakov@gmail.com>
2022-03-29 17:03:55 +0300

    Added new projects

---
+++ CSharp-Programming-basics/05WhileLoop/05AccountBalance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5._While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string input = Console.ReadLine();  // "400"
+
+            double totalAmount = 0;
+
+            while (input != "NoMoreMoney")
+            {
+
+                double currentAmount = double.Parse(input); // "400" -> 400
+
+                if (currentAmount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                totalAmount += currentAmount;
+
+                Console.WriteLine($"Increase: {currentAmount:f2}");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total: {totalAmount:f2}");
+
+        }
+    }
+}
---
+++ CSharp-Programming-basics/05WhileLoop/05WhileLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _5._Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            double allCakePieces = length * width;
+
+            while (allCakePieces > 0)
+            {
+                string input = Console.ReadLine();
+                if (input == "STOP")
+                {
+                    break;
+                }
+
+                int pieceOfCake = int.Parse(input);
+                allCakePieces -= pieceOfCake;
+            }
+
+            if (allCakePieces < 0)
+            {
+                Console.WriteLine("No More");
+                Console.WriteLine($"{Math.Abs(allCakePieces)}");
+            }
+            else if (allCakePieces >= 0)
+            {
+                Console.WriteLine($"{allCakePieces} pieces are left.");
+            }
+
+
+
+
+        }
+    }
+}
