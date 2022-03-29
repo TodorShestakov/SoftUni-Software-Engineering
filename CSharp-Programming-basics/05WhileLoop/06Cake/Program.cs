1705f42a6fb710d4e8a86fb855e30611d0941b02
Todor Shestakov <to.shestakov@gmail.com>
2022-03-29 19:09:01 +0300

    Adding new projects

---
+++ CSharp-Programming-basics/05WhileLoop/06Cake/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _6._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            int allCakePieces = length * width; // колко парчета торта ще имаме
+
+
+
+            while (allCakePieces > 0) // докато имаме парчета торта
+            {
+
+                string input = Console.ReadLine(); // прочитаме команда
+                if (input == "STOP") // ако получим команда стоп
+                {
+                    break;  // чупим цикъла
+                }
+                int pieceOfCake = int.Parse(input); // ако не е равна на стоп обаче ще получаваме парче торта
+                allCakePieces -= pieceOfCake; // вадим парче торта от общото количество
+
+            }
+
+            if (allCakePieces >= 0) // след като счупим цикъла и сме получили 0 или стоп, проверяваме дали имаме оставащи парчета торта
+            {
+                Console.WriteLine($"{allCakePieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(allCakePieces)} pieces more.");
+            }
+
+
+        }
+    }
+}
---
+++ CSharp-Programming-basics/05WhileLoop/06MaxNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _5._While_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+
+            int maxN = int.MinValue;
+
+            while (input != "Stop")
+            {
+                int currentNum = int.Parse(input);
+
+                if (currentNum > maxN)
+                {
+                    maxN = currentNum;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(maxN);
+        }
+    }
+}
