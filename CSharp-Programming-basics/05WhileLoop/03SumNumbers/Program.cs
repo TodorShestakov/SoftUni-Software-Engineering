0ff9ed2a8c8861615a7046b56d1b7b3466d9697f
Todor Shestakov <to.shestakov@gmail.com>
2022-03-29 13:07:28 +0300

    Added Sum Numbers project

---
+++ CSharp-Programming-basics/05WhileLoop/03SumNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (sum < number)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
