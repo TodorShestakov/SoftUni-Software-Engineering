16a8d5e782811c45471df150d458f71feb8454c6
Todor Shestakov <to.shestakov@gmail.com>
2022-03-29 21:54:00 +0300

    Added Graduation project

---
+++ CSharp-Programming-basics/05WhileLoop/Graduation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _8._Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+
+            double gradesSum = 0;
+            int grade = 1;
+            int failsCounter = 0;
+
+            while (grade <= 12)
+            {
+                double yearlyGrade = double.Parse(Console.ReadLine());
+
+                if (yearlyGrade < 4)
+                {
+                    failsCounter++;
+                    if (failsCounter == 2)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {grade} grade");
+                        break;
+                    }
+                    continue; 
+                }
+
+                gradesSum += yearlyGrade;
+                grade++;
+            }
+
+            double averageGrade = gradesSum / 12;
+            if (failsCounter < 2)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+            }
+                     
+        }
+    }
+}
