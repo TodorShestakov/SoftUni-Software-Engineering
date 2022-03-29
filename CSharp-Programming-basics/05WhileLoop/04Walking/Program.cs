d13e0f5c4b3cbf0d2c47c36c4ad83cec96777349
Todor Shestakov <to.shestakov@gmail.com>
2022-03-29 15:19:57 +0300

    Added Walking project

---
+++ CSharp-Programming-basics/05WhileLoop/04Walking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _4._Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int totalSteps = 0;
+
+            while (totalSteps < 10000)
+            {
+
+                string input = Console.ReadLine();
+
+
+                if (input == "Going home")
+                {
+                    int stepsLeft = int.Parse(Console.ReadLine());
+                    totalSteps += stepsLeft;
+                    break;
+                }
+
+                int currentSteps = int.Parse(input);
+                totalSteps += currentSteps;
+
+            }
+
+            if (totalSteps >= 10000)
+            {
+                Console.WriteLine($"Goal reached! Good job!");
+                int stepsOverTheGoal = totalSteps - 10000;
+                Console.WriteLine($"{stepsOverTheGoal} steps over the goal!");
+            }
+            else
+            {
+                int diff = Math.Abs(totalSteps - 10000);
+                Console.WriteLine($"{diff} more steps to reach goal.");
+            }
+        }
+    }
+}
