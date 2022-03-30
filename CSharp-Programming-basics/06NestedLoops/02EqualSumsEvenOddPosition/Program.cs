bf78d1afe54e825f74349bbc3bcb979787e920dd
Todor Shestakov <to.shestakov@gmail.com>
2022-03-30 13:26:07 +0300

    Added new projects

---
+++ CSharp-Programming-basics/06NestedLoops/02EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2._Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int num = start; num <= end; num++)
+            {
+                int currNum = num;
+                int evenSum = 0;
+                int oddSum = 0;
+                int count = 0;
+
+                while (currNum != 0)
+                {
+                    int digit = currNum % 10;
+                    if (count % 2 == 0)
+                    {
+                        evenSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+                    currNum /= 10; // 100001 / 10 = 10000
+                    count++;
+                }
+
+                if (evenSum == oddSum)
+                {
+                    Console.Write($"{num} ");
+                }
+            }
+        }
+    }
+}
