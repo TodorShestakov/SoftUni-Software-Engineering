99a721c993a823e143d439af5c50d53a23e76cde
Todor Shestakov <to.shestakov@gmail.com>
2022-03-30 11:29:19 +0300

    Added new projects

---
+++ CSharp-Programming-basics/06NestedLoops/01Clock/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nested_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <= 23; hour++)
+            {
+                for (int min = 0; min <= 59; min++)
+                {
+                    Console.WriteLine($"{hour:d2}:{min:d2}");  // :d2 показва 0 преди часовете и минутите не е включено в условието на задачата
+                }
+            }
+
+            //for (int day = 1; day <= 31; day++)
+            //{
+            //    Console.WriteLine($"{day}.10.2021");
+            //    for (int hour = 0; hour <= 23; hour++)
+            //    {
+            //        for (int min = 0; min <= 59; min++)
+            //        {
+            //            for (int sec = 0; sec <= 59; sec++)
+            //            {
+            //                Console.WriteLine($"{hour} : {min} : {sec}");
+            //            }
+            //        }
+            //    }
+            //}
+        }
+    }
+}
---
+++ CSharp-Programming-basics/06NestedLoops/01NumberPyramid/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Nested_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int targetNum = int.Parse(Console.ReadLine());
+
+            int row = 1;
+            int currNum = 1;
+            bool isEqual = false;
+
+            while (isEqual == false)
+            {
+                for (int i = 1; i <= row; i++)
+                {
+                    Console.Write($"{currNum} ");
+                    currNum++;
+                    if (currNum > targetNum)
+                    {
+                        isEqual = true;
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                row++;
+            }
+
+
+            //int targetNum = int.Parse(Console.ReadLine());
+
+            //int counter = 1;
+
+            //for (int i = 0; i < targetNum; i++)
+            //{
+
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write($"{counter} ");
+            //        if (counter == targetNum)
+            //        {
+            //            return;
+            //        }
+            //        counter++;
+            //    }
+
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
