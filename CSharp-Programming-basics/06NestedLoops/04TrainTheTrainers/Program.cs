018bdb44f973ac92369a7c50f603ce31343d97dd
Todor Shestakov <to.shestakov@gmail.com>
2022-03-30 19:10:19 +0300

    Added new projects

---
+++ CSharp-Programming-basics/06NestedLoops/04TrainTheTrainers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _4._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int juryCount = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            int gradeCount = 0;
+            double sumOfAllGrades = 0;
+
+            while (input != "Finish")
+            {
+                double sumOfGrades = 0;
+                for (int i = 1; i <= juryCount; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sumOfGrades += grade;
+                    gradeCount++;
+                    sumOfAllGrades += grade;
+                }
+                double averageForCurrentPresentation = sumOfGrades / juryCount;
+                Console.WriteLine($"{input} - {averageForCurrentPresentation:f2}.");
+                input = Console.ReadLine();
+            }
+
+            double finalAverageGrade = sumOfAllGrades / gradeCount;
+            Console.WriteLine($"Student's final assessment is {finalAverageGrade:f2}.");
+        }
+    }
+}
